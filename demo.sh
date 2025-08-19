dfb4e449bb9dfd24d512b7894381b415ac8e8edb
Sahil Singh <sahilhr24@gmail.com>
2025-08-19 12:04:02 +0530

    v3754

--- demo.sh
+++ demo.sh
@@ -548,7 +548,7 @@
 22-07-2025  11:41 AM         1,757,184 comsvcs.dll
 07-05-2022  10:49 AM           868,352 comuid.dll
 01-02-2002  07:02 PM           322,672 concrt140.dll
-14-08-2025  04:47 PM    <DIR>          config
+18-08-2025  04:19 PM    <DIR>          config
 22-07-2025  11:41 AM           765,952 configmanager2.dll
 22-07-2025  11:41 AM           114,688 ConfigureExpandedStorage.dll
 22-07-2025  11:41 AM         1,044,480 conhost.exe
@@ -3389,7 +3389,7 @@
 22-07-2025  11:45 AM    <DIR>          sl-SI
 22-07-2025  11:41 AM           192,512 slc.dll
 22-07-2025  11:41 AM            45,056 slcext.dll
-18-08-2025  09:39 AM    <DIR>          SleepStudy
+19-08-2025  10:03 AM    <DIR>          SleepStudy
 07-05-2022  10:49 AM            38,576 SlideToShutDown.exe
 07-05-2022  01:00 PM    <DIR>          slmgr
 07-05-2022  10:49 AM           142,904 slmgr.vbs
@@ -3491,7 +3491,7 @@
 07-05-2022  01:09 PM            61,440 SrpUxNativeSnapIn.dll
 07-05-2022  10:50 AM           237,568 srrstr.dll
 07-05-2022  10:50 AM            77,824 SrTasks.exe
-18-08-2025  11:50 AM    <DIR>          sru
+19-08-2025  11:06 AM    <DIR>          sru
 07-05-2022  10:50 AM            81,920 srumapi.dll
 07-05-2022  10:50 AM           262,144 srumsvc.dll
 22-07-2025  11:41 AM           142,040 srvcli.dll
@@ -4704,4 +4704,4 @@
 13-08-2025  10:30 AM           614,400 zipfldr.dll
 22-07-2025  11:41 AM            53,248 ztrace_maps.dll
             4562 File(s)  2,308,216,460 bytes
-             138 Dir(s)  224,615,514,112 bytes free
+             138 Dir(s)  223,690,395,648 bytes free
